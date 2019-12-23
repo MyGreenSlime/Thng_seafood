120202941ef97dfce1af1067125b0f66481cb273
iphatchanya <phatchanya.c@ku.th>
2019-12-23 22:19:38 +0700

    merge dondon to master

--- seafood_frontend/src/App.vue
+++ seafood_frontend/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <b-alert show>Default Alert</b-alert>
-
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-      <router-link to="/profile">profile</router-link>
-      <router-link to="/profile/address">address</router-link>
-      <router-link to="/profile/address/10">address detail</router-link>
-      <router-link to="/cart">cart</router-link>
-      <router-link to="/product">product</router-link>
-      <router-link to="/product/1">product detail</router-link>
-      <router-link to="/product/categories/5">product cate</router-link>
-      <router-link to="/checkout">checkout</router-link>
-    </div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <div id="nav">
+          <router-link to="/">Home</router-link> | 
+          <router-link to="/about">About</router-link>  | 
+          <router-link to="/profile">profile</router-link> | 
+          <router-link to="/profile/address">address</router-link> | 
+          <router-link to="/profile/address/10">address detail</router-link> | 
+          <router-link to="/cart">cart</router-link> | 
+          <router-link to="/product">product</router-link> | 
+          <router-link to="/product/1">product detail</router-link> | 
+          <router-link to="/product/categories/5">product cate</router-link> | 
+          <router-link to="/checkout">checkout</router-link>
+        </div>
+      </b-navbar-nav>
+    </b-navbar>
     <router-view />
   </div>
 </template>
@@ -33,7 +35,7 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 #nav a.router-link-exact-active {
