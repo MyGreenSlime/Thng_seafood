7e9acebf640c2ce4f54300170526113732e938a5
supachok768 <supachok768@gmail.com>
2019-12-24 01:39:43 +0700

    MaiRu

--- seafood_backend/config/database.js
+++ seafood_backend/config/database.js
@@ -1,65 +1,65 @@
 const Sequelize = require('sequelize');
-//const sequelize = new Sequelize('mysql://root:ZXCasdqwe1234!@119.59.114.59:3306/seafood');
-const sequelize = new Sequelize('mysql://root:example@localhost:3306/seafood');
+const sequelize = new Sequelize('mysql://root:ZXCasdqwe1234!@119.59.114.59:3306/seafood');
+//sconst sequelize = new Sequelize('mysql://root:example@localhost:3306/seafood');
 sequelize
-.authenticate()
-.then(() => {
-  console.log('Connection has been established successfully.');
-})
-.catch(err => {
-  console.error('Unable to connect to the database:', err);
-});
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const User = sequelize.define('User', {
-    // attributes
-    FirstName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    LastName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    Email : {
-        type : Sequelize.STRING,
-        allowNull: false,
-        unique : true
-    },
-    Password : {
-        type : Sequelize.TEXT,
-        allowNull: false
-    },
-    MobilePhone : {
-        type : Sequelize.STRING,
-        allowNull: false
-    },
-    Role : {
-        type : Sequelize.ENUM('admin', 'customer'),
-        allowNull : false
-    }
-  }, {
-    // options
+  // attributes
+  FirstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  LastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  Email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  Password: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  MobilePhone: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  Role: {
+    type: Sequelize.ENUM('admin', 'customer'),
+    allowNull: false
+  }
+}, {
+  // options
 })
 const Address = sequelize.define("Address", {
-    Address : {
-        type : Sequelize.TEXT,
-        allowNull : false
-    },
-    Provice : {
-        type : Sequelize.STRING
-    },
-    UserId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: User,
-          key: 'id'
-        } 
+  Address: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  Provice: {
+    type: Sequelize.STRING
+  },
+  UserId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
     }
+  }
 })
- 
+
 User.sync({ force: false })
 Address.sync({ force: false })
 
 module.exports = {
-    sequelize,User,Address,
-  }
+  sequelize, User, Address,
+}
--- seafood_frontend/src/views/account/profile.vue
+++ seafood_frontend/src/views/account/profile.vue
@@ -1,19 +1,54 @@
 <template>
   <div>
     <h1>Profile</h1>
+    <input type="button" value="Test Login" @click="login()" />
+    <input type="button" value="Test Register" @click="register()" />
+    <input type="button" value="Test Image To Base64S" @click="convertImage()" />
+    <input type="file" ref="file" />
+    <img :src="'data:image/jpg;base64' + image" alt="altText" />
   </div>
 </template>
 
 <script>
+import Auth from "@api/Auth";
+import base64 from "@libs/base64";
+import { async } from "q";
 export default {
   name: "Profile",
   data() {
     return {
-      key: "value"
+      image: undefined
     };
   },
   methods: {
-    name() {}
+    async login() {
+      let something = await Auth.login("ศุภโชค768@คน.ไทย", "123456");
+    },
+    register() {
+      console.log(
+        Auth.register(
+          "ศุภโชค768@คน.ไทย",
+          "123456",
+          "Test10",
+          "JOJO",
+          "0813819587"
+        )
+      );
+    },
+    async convertImage() {
+      console.log(this.$refs.file.files);
+      let base64 = await this.base64_encode(this.$refs.file.files[0]);
+      console.log(base64);
+      this.image = base64;
+    },
+    base64_encode(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
+    }
   }
 };
 </script>
